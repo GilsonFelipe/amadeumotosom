f29f85c6b093cf0958939a11156d3244fdabf33a
Gilson Felipe <33269580+GilsonFelipe@users.noreply.github.com>
2024-02-22 14:46:25 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 4000,
         infinite: true,
         arrows: false,
         responsive: [{
